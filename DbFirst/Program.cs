3e85eeb66c65cdb1035ded009c751c8d472d0a36
Patryk <s29443@pjwstk.edu.pl>
2026-06-06 20:40:43 +0200

    Add Program.cs

---
+++ DbFirst/Program.cs
@@ -0,0 +1,24 @@
+﻿using DbFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<HospitalContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
